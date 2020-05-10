7c6cd88484d80784f248511cca494159e55fdf80
Daniel Cerqueira <danielcerqueira@MacBook-Air-de-Daniel.local>
2020-05-10 17:39:35 -0300

    adicionando template engine Nunjucks

---
+++ data.js
@@ -0,0 +1,49 @@
+module.exports = [
+    {
+        id:'GykTLqODQuU', 
+        title: 'Formulário animado com JS Puro e CSS Animation', 
+        duration: '57 min', 
+        price: 'Free'
+
+    },
+
+    {
+        id:'ghTrp1x_1As', 
+        title: 'O que é API? REST e RESTFUL?', 
+        duration: '57 min', 
+        price: 'Free'
+
+    },
+
+    {
+        id:'vqrjFnq3-uo', 
+        title: 'Criando player de áudio', 
+        duration: '57 min', 
+        price: 'Free'
+
+    },
+
+    {
+        id:'HN1UjzRSdBk', 
+        title: 'Desvendando o CSS Grid na prática', 
+        duration: '57 min', 
+        price: 'Free'
+
+    },
+
+    {
+        id:'c7P03kkrEG8', 
+        title: 'Configurando o VS Code', 
+        duration: '57 min', 
+        price: 'Free'
+
+    },
+
+    {
+        id:'GykTLqODQuU', 
+        title: 'Formulário animado com JS Puro e CSS Animation', 
+        duration: '57 min', 
+        price: 'Free'
+
+    },
+]
