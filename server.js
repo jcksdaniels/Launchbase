6e159b20b0b231a11cebbd9ed5c51c986a8641c9
Daniel Cerqueira <danielcerqueira@MacBook-Air-de-Daniel.local>
2020-05-12 22:15:07 -0300

    setup do servidor com template engine Nunjucks

--- server.js
+++ server.js
@@ -36,6 +36,22 @@ server.get("/portfolio", function (req, res){
     return res.render("portfolio", { items: videos})
 })
 
+server.get('/video', function(req, res){
+    const id = req.query.id
+
+    const video = videos.find(function(video){
+        if(video.id == id){
+            return true
+        }
+    })
+
+    if (!video){
+        return res.send('Video not found!')
+    }
+
+    return res.render('video', {item: video})
+})
+
 server.listen(5000, function(){
     console.log('server is running')
 })
