863ed50149ccdcd75b07d02828d79c7ef16fc144
Daniel Cerqueira <danielcerqueira@MacBook-Air-de-Daniel.local>
2020-05-12 22:18:36 -0300

    projeto atualizado

--- public/scripts.js
+++ public/scripts.js
@@ -5,16 +5,11 @@ for(let card of cards){
       
     card.addEventListener('click', function(){
         const videoId = card.getAttribute('id')
-        modalOverlay.classList.add('active')
-        modalOverlay.querySelector('iframe').src=`https://youtube.com/embed/${videoId}`;
+        window.location.href=`/video?id=${videoId}`
+        
 
     })
 }
 
 
-document.querySelector('.close-modal').addEventListener('click', function(){
-    modalOverlay.classList.remove('active')
-    modalOverlay.querySelector('iframe').src = '';
-
-});
 
